29e00ab49eef0b0a6b16577926d07dc5cbe8fa91
rdrachenberg <31778670+rdrachenberg@users.noreply.github.com>
2018-01-10 22:23:28 -0500

    working app v102

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,26 +8,33 @@ $(document).ready(function(){
 	    storageBucket: "trainscheduler-1.appspot.com",
 	    messagingSenderId: "827220560124"
 	 };
-	 
+	
+	 // initialized firebase app
 	firebase.initializeApp(config);
+	
 	// create a var of btn to represent my button with an id of submit	
 	var btn = $("#submit");
+	
 	// creates a variable of database and sets it equal to our firebase database
 	var database = firebase.database();
 
 	// this is our on click listener function to gather the inputed information 
 	// and push it to our firebase database
 	$(btn).on("click", function(event) {
+	
+		// stops default behavior 
+		event.preventDefault();
 
-	event.preventDefault();
-
+		// creates directories and inputs the values of the selected
+		 // jQuery elements into those directories
 	    name = $("#inlineFormInput").val().trim();
 	    destination = $("#destination").val().trim();
 	    arrivalTime = $("#arrivalTime").val().trim();
 	    frequency = $("#frequency").val().trim();
 	    nextArrival = $("#arrivalTime").val().trim();
 	    
-	      
+	    // creates directories  and then pushes the value of the second variable
+	     // information to our variable of database, which is our firebase database.  
 	    database.ref().push({
 	      	trainName: name,
 	        destination: destination,
@@ -35,33 +42,34 @@ $(document).ready(function(){
 	        frequency: frequency,
 	        nextArrival: nextArrival
 	        
-	        
-	     });
-
-	      console.log("Train Succesfully Added!");
-	    
 	    });
+	    // sets timeout to reset the page and then therefore updateing the data
+		setTimeout(function(){
+       	location.reload();
+   		},1000);
+	});
+
+	// creates an event listener to monitor when a chiled element 
+	// is added and then execute a function. 
 	database.ref().on('child_added', function(snapshot){
+	
 	// pulls the value of frequency from firebase and creates a variable of tFrequency
     var tFrequency = snapshot.val().frequency;
+    
     // first train arrival 
     var firstTrainArrival = snapshot.val().arrivalTime;
-
+    
     // First Time (pushed back 1 year to make sure it comes before current time)
     var firstTrainArrivalConverted = moment(firstTrainArrival, "hh:mm").subtract(1, "years");
-    console.log(firstTrainArrivalConverted);
-
+    
     // creates a variable of the current time named currentTime
     var currentTime = moment();
     
-
     // mesures the difference between the subtracted year and now
     var diffTime = moment().diff(moment(firstTrainArrivalConverted), "minutes");
-    console.log("DIFFERENCE IN TIME: " + diffTime);
 
     // Time apart (remainder)
     var tRemainder = diffTime % tFrequency;
-    console.log(tRemainder);
 
     // create a variable  and equals it to the frequuency minus how much time 
     // until the next frequency interval which will be equal to the  Minutes Until
@@ -71,6 +79,7 @@ $(document).ready(function(){
     // creates a vaiable equal to now plus the minutes left until the
      // Next Train arrives which will return the time of arrival
     var arrivingTrain = moment().add(tMinutesTillTrain, "minutes");
+    
     // additional vaiable to clean up the variable and will display same information
     var nextArrival = moment(arrivingTrain).format("hh:mm");
 	
@@ -83,8 +92,17 @@ $(document).ready(function(){
 	addRow.append("<td>" + (snapshot.val().arrivalTime) + "</td>");
 	addRow.append('<td id="nextArrival">' + (nextArrival) + "</td>");
 	addRow.append('<td id="nextMin">' + (tMinutesTillTrain) + "</td>");
+
+	
 	// select the class of table and append all information to a new table row which is created in the add row variable
 	$(".table").append(addRow);
 	});
+
+	// sets timeout to reset the page and then therefore updateing the data
+	setTimeout(function(){
+       location.reload();
+   },60000);
+
+    
 });
 
